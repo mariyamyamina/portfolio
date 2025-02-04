899a4122ac7e9684e7074160a26fc9691496cf44
mariyamyamina <mariyamyamina@gmail.com>
2025-02-04 23:41:55 +0530

    modified

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -8,21 +8,19 @@ const Footer = () => {
     <footer className="footer-container">
       <div className="contact-details">
         <p><b>Contact Details</b></p>
-        <p>4/20 New Street, Chennai</p>
-        <p>+91-8940767376</p>
-        <p>mariyamyamina1999@gmail.com</p>
+        <p>mariyamyamina@gmail.com</p>
       </div>
       <div className="contact-icons">
         <a href="https://github.com/MariyamYamina" target="_blank" rel="noopener noreferrer">
           <FontAwesomeIcon icon={faGithub} size="2x" />
         </a>
-        <a href="https://www.linkedin.com/in/mariyam-yamina/" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.linkedin.com/in/mariyamyamina/" target="_blank" rel="noopener noreferrer">
           <FontAwesomeIcon icon={faLinkedin} size="2x" />
         </a>
         <a href="https://twitter.com/MariyamYamina" target="_blank" rel="noopener noreferrer">
           <FontAwesomeIcon icon={faTwitter} size="2x" />
         </a>
-        <a href="https://www.instagram.com/its_yami_21/" target="_blank" rel="noopener noreferrer">
+        <a href="" target="_blank" rel="noopener noreferrer">
           <FontAwesomeIcon icon={faInstagram} size="2x" />
         </a>
       </div>
