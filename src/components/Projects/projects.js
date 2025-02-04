a8d46bb692952c13ade9d14374ff91793a6abd01
mariyamyamina <mariyamyamina@gmail.com>
2025-02-04 23:57:00 +0530

    modified

--- src/components/Projects/projects.js
+++ src/components/Projects/projects.js
@@ -14,37 +14,37 @@ const Projects = () => {
         <h2 align="center">LATEST PROJECTS</h2>
         <div className="project-box">
           <div className="project-img">
-            <a href="https://mariyamyamina.github.io/e-commerce/" target="_blank">
+            <a href="https://github.com/mariyamyamina/e-commerce" target="_blank">
               <img src={project1} alt="E-commerce" />
               <p>E-Commerce website</p>
             </a>
           </div>
           <div className="project-img">
-            <a href="">
+            <a href="https://github.com/mariyamyamina/blog-website">
               <img src={blogs} alt="blogs" />
               <p>Blogs website</p>
             </a>
           </div>
           <div className="project-img">
-            <a href="">
+            <a href="https://github.com/mariyamyamina/Amazon-clone">
               <img src={amazon} alt="amazon clone" />
               <p>Amazon clone app</p>
             </a>
           </div>
           <div className="project-img">
-            <a href="">
+            <a href="https://github.com/mariyamyamina/spotify-clone">
               <img src={spotify} alt="spotify clone" />
               <p>Spotify</p>
             </a>
           </div>
           <div className="project-img">
-            <a href="https://github.com/mariyamyamina/internpe">
+            <a href="https://github.com/mariyamyamina/FoodDelivery">
               <img src={project5} alt="Internpe Project" />
               <p>Food delivery website</p>
             </a>
           </div>
           <div className="project-img">
-            <a href="https://mariyamyamina.github.io/Bibliolater/">
+            <a href="https://github.com/mariyamyamina/Bibliolater">
               <img src={project6} alt="Bibliolater" />
               <p>Book Management App</p>
             </a>
